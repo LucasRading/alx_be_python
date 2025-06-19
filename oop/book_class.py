1231ae352c0e8263141961a100838700f3d5c0ea
Lucas Rading <radinglucas@gmail.com>
2025-06-20 00:07:33 +0300

    Implement Book class with __del__ method

---
+++ oop/book_class.py
@@ -0,0 +1,11 @@
+# book_class.py
+
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        print(f"Deleting {self.title}")
+
